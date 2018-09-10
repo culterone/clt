bef34ea0f075036509b637d5c0c1a3a828b8daca
culterone <mafelo@azet.sk>
2018-09-10 10:11:42 +0200

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,74 @@
+from flask import Flask, render_template, request, jsonify, json, Blueprint
+from flask_sqlalchemy import SQLAlchemy
+from serverside.serverside_table import ServerSideTable
+from serverside import table_schemas
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secretkey57'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://monty:kc532*LC@10.99.99.11/test1'
+db = SQLAlchemy(app)
+
+class table2(db.Model):
+   __tablename__ = 'table2'
+   cola = db.Column('A', db.String(2))
+   colb = db.Column('B', db.String(2))
+   colc = db.Column('C', db.Integer, primary_key=True)
+   cold = db.Column('D', db.Integer)
+
+   def __init__(self, cola, colb, colc, cold):
+      self.cola = cola
+      self.colb = colb
+      self.colc = colc
+      self.cold = cold
+      pass
+
+   @property
+   def serialize(self):
+      return {
+         'cola': self.cola,
+         'colb': self.colb,
+         'colc': self.colc,
+         'cold': self.cold
+      }
+
+# DB COUNT ROWS
+rowsCount = table2.query.count()
+
+# DB query and serialize
+tick = table2.query.all()
+data=[i.serialize for i in tick]
+
+
+@app.route('/data')
+def get_data():
+   return render_template('data.html')
+
+
+# merging dictionaries to add draw, recordsTotal..
+x = {'draw':1,'recordsTotal':rowsCount,'recordsFiltered':10}
+y = dict(data=[i.serialize for i in tick])
+
+z = y.copy()
+z.update(x)
+
+@app.route("/api/result")
+def result_json():
+   return jsonify(z)
+
+
+# Sergio's part
+
+class TableBuilder(object):
+
+   def collect_data_clientside(self):
+      return {'data': data}
+
+   def collect_data_serverside(self, request):
+      columns = table_schemas.SERVERSIDE_TABLE_COLUMNS
+      return ServerSideTable(request, data, columns).output_result()
+
+
+#@main.route("/serverside_table")
+#def serverside_table():
+#   print("Hello")
+#   return render_template("data.html")
---
+++ serverside/table_schemas.py
@@ -0,0 +1,30 @@
+SERVERSIDE_TABLE_COLUMNS = [
+    {
+        "data_name": "cola",
+        "column_name": "A",
+        "default": "",
+        "order": 1,
+        "searchable": True
+    },
+    {
+        "data_name": "colb",
+        "column_name": "B",
+        "default": "",
+        "order": 2,
+        "searchable": True
+    },
+    {
+        "data_name": "colc",
+        "column_name": "C",
+        "default": 0,
+        "order": 3,
+        "searchable": False
+    },
+    {
+        "data_name": "cold",
+        "column_name": "D",
+        "default": 0,
+        "order": 4,
+        "searchable": False
+    }
+]
